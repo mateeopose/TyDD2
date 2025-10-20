eef849dafe04f087b2166756cc9d29186a0da613
mateeopose <mateoposeoo7@gmail.com>
2025-10-20 16:30:39 -0300

    Inicio de la parte A

--- Parte A/Sumador_Completo.vhd
+++ Parte A/Sumador_Completo.vhd
@@ -1,1 +1,10 @@
-entity sumador_completo is 
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Sumador_Completo is
+	port( A: in std_logic;
+			B: in std_logic;
+			Cin: in std_logic;
+			Cout: out std_logic;
+			
+	
